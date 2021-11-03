403d049638a087144eb136d3d6a34605d1521ed2
Mr-Ravil <ravil.galiev.2000@mail.ru>
2021-11-04 01:35:11 +0300

    correction main class

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static int BLOCK_SIZE = 1000;
     private static int LIMIT = (int) 1e8;
     private static int ATTEMPTS_COUNT = 5;
+
+    private static String timesToString(List<Long> times){
+        String timesString = times.stream().map(String::valueOf).collect(Collectors.joining(" ms, "));
+        return timesString + " ms";
+    }
 
     public static void main(String[] args) {
         long startTime;
@@ -14,37 +21,43 @@ public class Main {
         long allParTime = 0;
         long allSeqTime = 0;
 
-        QuickSort parallelQuickSort = new ParallelQuickSort();
-        QuickSort sequenceQuickSort = new SequenceQuickSort();
-
-
-        System.out.println("Block size: " + ((ParallelQuickSort) parallelQuickSort).getBlockSize());
+        System.out.println("Block size: " + BLOCK_SIZE);
         System.out.println("Array size: " + LIMIT);
         System.out.println("Attempts count: " + ATTEMPTS_COUNT + "\n\n");
 
+        QuickSort parallelQuickSort = new ParallelQuickSort(BLOCK_SIZE);
+        QuickSort sequenceQuickSort = new SequenceQuickSort();
+
+        List<Long> parTimes = new ArrayList<>();
+        List<Long> seqTimes = new ArrayList<>();
+
         for (int i = 0; i < ATTEMPTS_COUNT; i++) {
-            System.out.println("Sort number: " + i);
             List<Integer> parList = new Random().ints().limit(LIMIT).boxed().collect(Collectors.toList());
             List<Integer> seqList = new ArrayList<>(parList);
 
             startTime = System.nanoTime();
             parallelQuickSort.sort(parList);
             endTime = System.nanoTime();
-            System.out.println("Parallel sorting time: " + (endTime - startTime) + " nanoseconds.");
-            allParTime += endTime - startTime;
+            long parTime = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+            parTimes.add(parTime);
+            allParTime += parTime;
 
             startTime = System.nanoTime();
             sequenceQuickSort.sort(seqList);
             endTime = System.nanoTime();
-            System.out.println("Sequence sorting time: " + (endTime - startTime) + " nanoseconds.\n");
-            allSeqTime += endTime - startTime;
+            long seqTime = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+            seqTimes.add(seqTime);
+            allSeqTime += seqTime;
         }
 
         long aveParTime = allParTime / ATTEMPTS_COUNT;
         long aveSeqTime = allSeqTime / ATTEMPTS_COUNT;
 
-        System.out.println("\nAverage parallel sorting time: " + aveParTime + " nanoseconds.");
-        System.out.println("Average sequence sorting time: " + aveSeqTime + " nanoseconds.");
-        System.out.println("\nParallel sort " + ((double) aveSeqTime / (double) allParTime) + " times faster than Sequence sort.");
+        System.out.println("Parallel sorting times: " + timesToString(parTimes) + ".");
+        System.out.println("Sequence sorting times: " + timesToString(seqTimes) + ".");
+
+        System.out.println("\nAverage parallel sorting time: " + aveParTime + " ms.");
+        System.out.println("Average sequence sorting time: " + aveSeqTime + " ms.");
+        System.out.println("\nParallel sort " + ((double) aveSeqTime / (double) aveParTime) + " times faster than Sequence sort.");
     }
 }
